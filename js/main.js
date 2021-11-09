3f788209f7f240903babb2c0b9b6a9f1f79b0360
fannyrosen <fanny.rosen@hotmail.com>
2021-11-09 14:49:03 +0100

    finished removel and linethrough

--- js/main.js
+++ js/main.js
@@ -1,36 +1,87 @@
+class ToDo {
+  constructor(task, completed) {
+    this.task = task;
+    this.completed = completed;
+  }
+}
+
 window.onload = function () {
   createList();
-  addToDos();
+  let addToDoButton = document.getElementById("addToDo");
+  addToDoButton.addEventListener("click", addToDos);
 };
-//global variabel
-let ul = document.createElement("ul");
-ul.id = "list";
+
+let t = new ToDo("Buy ticket", false);
+let t2 = new ToDo("Book hotel", false);
+let t3 = new ToDo("Feed cat", false);
+let t4 = new ToDo("Send email", false);
+let t5 = new ToDo("Make appointment", false);
+let t6 = new ToDo("Water plant", false);
+
+let toDos = [t, t2, t3, t4, t5, t6];
 
 function createList() {
-  let createList = [
-    "Buy ticket",
-    "Book hotel",
-    "Feed cat",
-    "Send email",
-    "Make appointment",
-    "Water plant",
-  ];
+  document.getElementById("listContainer").innerHTML = "";
+  let ul = document.createElement("ul");
+  ul.id = "list";
 
-  for (let i = 0; i < createList.length; i++) {
+  for (let i = 0; i < toDos.length; i++) {
     let li = document.createElement("li");
-    li.innerHTML = createList[i];
+    li.innerHTML = toDos[i].task;
+    li.addEventListener("click", () => {
+      handleClick(i);
+    });
+
+    if (toDos[i].completed) {
+      li.className = "markeddone";
+    }
+
     ul.appendChild(li);
+
+    let button = document.createElement("button");
+    button.innerHTML = "x";
+    button.id = "remove";
+    button.addEventListener("click", () => {
+      removeItem(i);
+    });
+
+    li.appendChild(button);
   }
   document.getElementById("listContainer").appendChild(ul);
 }
+function removeItem(i) {
+  toDos.splice(i, 1);
+
+  createList();
+}
+
+function handleClick(i) {
+  console.log("Klickade!");
+  //1. Hitta det i:te objektet i listan
+  //2. Ändra completed på objektet till true
+  //3. Anropa createList-funktionen
+  toDos[i].completed = !toDos[i].completed;
+  console.log(toDos);
+  createList();
+}
 
 function addToDos() {
-  let addToDoButton = document.getElementById("addToDo");
   let inputField = document.getElementById("addItem");
+  let newToDo = new ToDo(inputField.value, false);
+  toDos.push(newToDo);
 
-  addToDoButton.addEventListener("click", function () {
-    let newToDo = document.createElement("li");
-    newToDo.innerHTML = inputField.value;
-    ul.appendChild(newToDo);
-  });
+  document.getElementById("addItem").value = "";
+
+  createList();
 }
+
+// let newToDo = document.createElement("li");
+// newToDo.innerHTML = inputField.value;
+// ul.appendChild(newToDo);
+// inputField.value = "";
+// newToDo.addEventListener("click", function () {
+//   newToDo.style.textDecoration = "line-through";
+// });
+// newToDo.addEventListener("dblclick", function () {
+//   itemContainer.removeChild(newToDo);
+// });
